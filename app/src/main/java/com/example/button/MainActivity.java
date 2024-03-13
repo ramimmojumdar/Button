be28748a132d39a8c72b07005b1ef98d0e3ca76f
ramimmojumdar <expertramim@gmial.com>
2024-03-13 22:37:43 +0600

    new

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,21 +24,63 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.btn2);
         tvDisplay = findViewById(R.id.tvDisplay);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tvDisplay.setText("Login Button is Clicked");
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tvDisplay.setText("LogOut Button is Clicked");
-            }
-        });
 
 
 
+/**
+        Handler handler = new Handler();
+
+        btn1.setOnClickListener(handler);
+        btn2.setOnClickListener(handler);
+
+ **/
+
+
+//=================================================
+//=================================================
+
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                tvDisplay.setText("Login Button is Clicked");
+//            }
+//        });
+//
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                tvDisplay.setText("LogOut Button is Clicked");
+//            }
+//        });
+
+//=================================================
+//=================================================
+
+    }// onCreate Bundle
+
+
+    public void clickedMessage (View view){
+        if (view.getId()==R.id.btn1){
+            tvDisplay.setText("Login Button is Clicked");
+        }if (view.getId()==R.id.btn2){
+            tvDisplay.setText("Logout Button is Clicked");
+        }
     }
-}
+
+/**
+    class Handler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId()==R.id.btn1){
+                tvDisplay.setText("Login Button is Clicked");
+            }if (view.getId()==R.id.btn2){
+                tvDisplay.setText("Logout Button is Clicked");
+            }
+
+        }
+    }
+ **/
+
+
+} //AppCompatActivity
